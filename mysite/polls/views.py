d49df7160801fdf6d91cb0a78c6eb1ae0f2a6d81
Kin <winterheat@gmail.com>
2016-02-15 18:22:32 -0800

    adding some views in polls view.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -10,3 +10,16 @@ def index(request):
     arr = [[x+y for x in [1,2,3,4]] for y in [10,100,1000]]
     s = json.dumps(arr, sort_keys=True, indent=4)
     return HttpResponse("<pre>Hello, world. You're at the polls index.\n" + s)
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+    
